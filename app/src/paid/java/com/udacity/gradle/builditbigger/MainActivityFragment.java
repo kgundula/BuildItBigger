c41957a11a987e9a4406bd5f5c27ee396938802f
kgundula <kgundula@gmail.com>
2016-11-14 21:51:46 +0200

    Added free and paid flavors

--- app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -9,6 +9,7 @@ import android.view.ViewGroup;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
+import com.udacity.gradle.builditbigger.R;
 
 
 /**
@@ -24,17 +25,6 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
 
-        AdView mAdView = (AdView) root.findViewById(R.id.adView);
-
-        MobileAds.initialize(getContext(), getString(R.string.banner_ad_unit_id));
-
-        // Create an ad request. Check logcat output for the hashed device ID to
-        // get test ads on a physical device. e.g.
-        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
-        AdRequest adRequest = new AdRequest.Builder()
-                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-                .build();
-        mAdView.loadAd(adRequest);
         return root;
     }
 }
